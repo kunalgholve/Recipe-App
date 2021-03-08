0062f4da6962b5428eee81487cff134e173b16c4
kunal <gkunal7996@gmail.com>
2021-03-08 17:53:12 +0530

    commitecss

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import style from './recipe.module.css';
 const Recipe=({title,calories,image,ingredients})=>{
   return(
-    <div>
+    <div className={style.recipe}>
       <h1>{title}</h1>
-      <p>calories:{calories}</p>
-      <img src={image} alt='recipeimg'/>
       <ol>
       Ingredients : {ingredients.map((ingredients) => (
           <li>{ingredients.text}</li>
         ))}
       </ol>
+      <p>calories:{calories}</p>
+      <img className={style.image} src={image} alt='recipeimg'/>
     </div> 
   )
 }
